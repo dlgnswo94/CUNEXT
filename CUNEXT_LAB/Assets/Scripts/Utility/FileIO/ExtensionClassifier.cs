57aa1e7245077d85950abd8bf86f02bb14776772
dlgnswo94 <dlgnswo94@gmail.com>
2022-01-21 17:29:26 +0900

    ExtensionClassifier.cs 추가

---
+++ CUNEXT_LAB/Assets/Scripts/Utility/FileIO/ExtensionClassifier.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using static System.IO.Directory;
+using UnityEngine;
+using CUNEXT.Utility.Enums;
+
+namespace CUNEXT
+{
+    namespace Utility
+    {
+        namespace FileIO
+        {
+            public class ExtensionClassifier
+            {
+                public static EFileNameExtension Classifying(string path = "", string fileName = "")
+                {
+                    if (!Exists(path))
+                    {
+                        LogSender.SendLog(ELogType.Warning, "A path that doesn't exist. Can't classifying. / path : {0} ", path);
+                        return EFileNameExtension.None;
+                    }
+
+                    string strExtension = Path.GetExtension(fileName);
+                    strExtension = strExtension.Remove(0, 1); // Remove '.' for parsing.
+
+                    EFileNameExtension extension = EFileNameExtension.None;
+
+                    if (!Enum.IsDefined(typeof(EFileNameExtension), strExtension))
+                    {
+                        LogSender.SendLog(ELogType.Error, "This extension does not exist in the EFileNameExtension.", null);
+                        return extension;
+                    }
+
+                    extension = (EFileNameExtension)System.Enum.Parse(typeof(EFileNameExtension), strExtension);
+                    return extension;
+                }
+            }
+        }
+    }
+}
