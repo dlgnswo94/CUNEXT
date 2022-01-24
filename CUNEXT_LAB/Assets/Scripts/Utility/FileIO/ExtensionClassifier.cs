802ab7d5405d82482d6723c29ff9431ccd891781
dlgnswo94 <dlgnswo94@gmail.com>
2022-01-24 16:20:46 +0900

    ExtensionClassifier 수정

--- CUNEXT_LAB/Assets/Scripts/Utility/FileIO/ExtensionClassifier.cs
+++ CUNEXT_LAB/Assets/Scripts/Utility/FileIO/ExtensionClassifier.cs
@@ -11,15 +11,9 @@ namespace CUNEXT
         {
             public class ExtensionClassifier
             {
-                public static EFileNameExtension Classifying (string path = "", string fileName = "")
+                public static EFileNameExtension Classifying (FileInfo fileInfo)
                 {
-                    if (!Exists(path))
-                    {
-                        LogSender.SendLog(ELogType.Warning, "A path that doesn't exist. Can't classifying. / path : {0} ", path);
-                        return EFileNameExtension.None;
-                    }
-
-                    string strExtension = Path.GetExtension(fileName);
+                    string strExtension = fileInfo.Extension;
                     strExtension = strExtension.Remove(0, 1); // Remove '.' for parsing.
 
                     EFileNameExtension extension = EFileNameExtension.None;
